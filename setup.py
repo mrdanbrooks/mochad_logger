595848f5ffc15b2fb9feb6760abb73f0f242103e
dan brooks <dan@danbrooks.net>
2014-11-08 00:35:10 -0500

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup
+from setuptools import setup
+
+setup(name='mochad_logger',
+      version='0.0.1',
+      description='A Logging Utility for Mochad',
+      author='Dan Brooks',
+      author_email='dan@cs.uml.edu',
+      url='http://www.github.com/mrdanbrooks/mochad_logger',
+      py_modules=['mochad_logger'],
+      )
