2b06586ad2aa25bcb089c58732cdfaa2b4da4122
dan brooks <dan@danbrooks.net>
2014-11-14 15:46:59 -0500

    updates setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@
 # from distutils.core import setup
 from setuptools import setup
 
+
 setup(name='mochad_logger',
       version='0.0.1',
       description='A Logging Utility for Mochad',
       author='Dan Brooks',
       author_email='dan@cs.uml.edu',
       url='http://www.github.com/mrdanbrooks/mochad_logger',
-      py_modules=['mochad_logger'],
+      scripts=['mochad_logger'],
+      data_files=[('/etc/mochad_logger',['config/mochad_logger.conf'])],
+      install_requires=['twisted']
       )
