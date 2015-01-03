7f2573c5f22f9638c52d6f5d86bf26ba89e57dec
dan brooks <dan@danbrooks.net>
2015-01-03 16:09:54 -0500

    0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='mochad_logger',
-      version='0.0.4',
+      version='0.0.5',
       description='A Logging Utility for Mochad',
       author='Dan Brooks',
       author_email='dan@cs.uml.edu',
